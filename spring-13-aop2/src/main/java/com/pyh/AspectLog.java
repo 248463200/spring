47839ac965f9975df1317d6ae351cd58a47d8351
peng_yaohui <248463200@qq.com>
2021-01-18 22:25:59 +0800

    配置类自定义来实现AOP的前置和后置

---
+++ spring-13-aop2/src/main/java/com/pyh/AspectLog.java
@@ -0,0 +1,11 @@
+package com.pyh;
+
+public class AspectLog {
+    public void before(){
+        System.out.println("=======执行方法前=======");
+    }
+
+    public void after(){
+        System.out.println("=======执行方法后=======");
+    }
+}
